97de54e806a5e116a7ebbe184aa72e37aa287820
CharlesPeterMcCarthy <charles@studybundles.com>
2018-10-02 16:41:17 +0100

    Formed Skate Sessions List

--- screens/SkateSessions.js
+++ screens/SkateSessions.js
@@ -6,30 +6,180 @@ import {
   View,
   Text,
   AsyncStorage,
-  BackHandler
+  BackHandler,
+  ActivityIndicator,
+  Button,
+  FlatList,
+  TouchableOpacity
 } from 'react-native';
+import moment from 'moment';
 
 type Props = {};
 export default class SkateSessions extends Component<Props> {
   static navigationOptions = {
     title: 'Skate Sessions',
-    headerLeft: null
+    headerLeft: null,
+    headerStyle: {
+      backgroundColor: '#555'
+    },
+    headerTitleStyle: {
+      color: '#a2c2d8'
+    }
   };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gettingSessions: false,
+      errorMsg: '',
+      error: false,
+      sessions: []
+    }
+  }
 
   componentWillMount() {
     BackHandler.addEventListener('hardwareBackPress', () => {
       return true;
     });
+
+    this._getSessions();
+  }
+
+  _getSessions = () => {
+    fetch('{{ API_URL }}', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({})
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this._checkResponse(responseJson)
+      return responseJson
+    })
+    .catch((error) => console.error(error))
+  }
+
+  _checkResponse(response) {
+    this.setState({ gettingSessions: false })
+
+    if (response.isOk) this.setState({ sessions: response.sessions });
+    else this.setState({ error: true, errorMsg: response.displayError });
+  }
+
+  _keyExtractor = (item, index) => index.toString();
+
+  _renderItem = ({item, index}) => {
+    return  (
+      <SessionPanel
+        item={item}
+        index={index}
+        onPressItem={this._viewSession}
+      />
+    );
+  }
+
+  _viewSession = (index) => {
+    this.props.navigation.navigate('Session', { sessionID: this.state.sessions[index].sessionID });
   }
 
   render() {
-    return (
+    const spinner = this.state.gettingSessions ? <ActivityIndicator style={styles.spinner} size='large' /> : null;
+    const error = this.state.error ? (
       <View>
+        <Text>{this.state.errorMsg}</Text>
+      </View>
+    ) : null;
+
+    return (
+      <View style={styles.container}>
+        {spinner}
+        {error}
+        <FlatList
+          data={this.state.sessions}
+          keyExtractor={this._keyExtractor}
+          renderItem={this._renderItem}
+          style={styles.sessionList}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+class SessionPanel extends Component<Props> {
+  _onPress = () => {
+    this.props.onPressItem(this.props.index);
+  }
 
+  render() {
+    const item = this.props.item;
+    const timeAgo = moment(item.start).fromNow();
+    const timeBetween = moment.duration(moment(item.end).diff(moment(item.start)));
+    const skateLength = Math.floor(timeBetween.asMinutes()) + " minutes " + (timeBetween.asSeconds() % 60) + " seconds.";
+
+    return (
+      <TouchableOpacity
+        style={styles.sessionPanel}
+        onPress={this._onPress}
+      >
+        <View style={styles.sessionHeader}>
+          <View style={{flex:1}}>
+            <Text style={styles.headerText}>#{item.sessionID}</Text>
+          </View>
+          <View style={{flex:1}}>
+            <Text style={[styles.timeAgo, styles.headerText]}>{timeAgo}</Text>
+          </View>
+        </View>
+        <View>
+          <Text style={styles.detail}>Skate Distance: {item.distance} KM</Text>
+          <Text style={styles.detail}>Skate Length: {skateLength}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#a2c2d8',
+    flex: 1
+  },
+  spinner: {
+    marginTop: 20
+  },
+  sessionList: {
+    marginTop: 5,
+  },
+  sessionPanel: {
+    padding: 10,
+    marginHorizontal: 5,
+    marginVertical: 2,
+    borderRadius: 4,
+    backgroundColor: '#555'
+  },
+  timeAgo: {
+    textAlign: 'right'
+  },
+  sessionHeader: {
+    flexDirection: 'row',
+    flex: 1,
+    borderBottomWidth: 0.5,
+    borderBottomColor: '#a2c2d8',
+    paddingBottom: 10,
+    marginBottom: 10
+  },
+  times: {
+    marginTop: 10
+  },
+  detail: {
+    color: '#CCC'
+  },
+  headerText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#FFF'
+  }
 })
