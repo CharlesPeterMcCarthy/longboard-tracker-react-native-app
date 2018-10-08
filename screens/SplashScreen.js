b5091fcf2c4b52f7f5ce9820525e7123591deac4
CharlesPeterMcCarthy <charles@studybundles.com>
2018-10-08 11:44:45 +0100

    Added Image To SplashScreen & Simulated Loading

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -4,36 +4,65 @@ import React, { Component } from 'react';
 import {
   View,
   AsyncStorage,
-  ActivityIndicator,
-  StyleSheet
+  StyleSheet,
+  Image,
+  Dimensions
 } from 'react-native';
+import { ProgressCircle }  from 'react-native-svg-charts';
 
 export default class SplashScreen extends Component<Props> {
   constructor(props) {
     super(props);
     this._checkAuth();
+
+    this.state = {
+      progress: 0.0,
+      progressColor: '#474747',
+      progressStrokeWidth: 10
+    }
   }
 
   _checkAuth = async () => {
     const loggedIn = await AsyncStorage.getItem('loggedIn');
 
-    this.props.navigation.navigate(loggedIn === "true" ? 'Auth' : 'Login');
+    this._showLoading();
+
+    setTimeout(() => {
+      this.props.navigation.navigate(loggedIn === "true" ? 'Auth' : 'Login');
+    }, 2000)
   };
+
+  _showLoading = () => {
+    for (let i = 0; i < 9; i++) setTimeout(() => {this.setState({progress: ((i + 1) / 10)})}, (150 * (i + 1)));
+
+    setTimeout(() => {this.setState({progress: 1, progressColor: '#00a300', progressStrokeWidth: 25})}, 1600);
+  }
 
   render() {
     return (
-      <View>
-        <ActivityIndicator size='large' style={styles.spinner} />
+      <View style={styles.container}>
+        <Image style={styles.image} source={require('../images/logo.jpg')} />
+        <ProgressCircle
+          style={{ height: 80, width: 80 }}
+          progress={ this.state.progress }
+          progressColor={ this.state.progressColor }
+          backgroundColor={'#ccc'}
+          strokeWidth={ this.state.progressStrokeWidth }
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  spinner: {
-    marginTop: 200,
+  container: {
+    backgroundColor: 'white',
+    alignItems: 'center',
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: 'center'
+  },
+  image: {
+    width: Dimensions.get('window').width * 0.8,
+    height: Dimensions.get('window').width * 0.8
   }
 })
